aa83aa878710e60677a2d39cfd470dd59319d1fa
Fabian Schuh <mail@xeroc.org>
2016-07-18 11:53:44 +0200

    more work

--- piston/steem.py
+++ piston/steem.py
@@ -11,12 +11,9 @@ from piston.utils import (
     derivePermlink,
 )
 from piston.wallet import Wallet
-from piston.configuration import Configuration
+from .storage import configStorage as config
 import logging
 log = logging.getLogger(__name__)
-
-#: Configuration from local user settings
-config = Configuration()
 
 #: Default settings
 if "node" not in config or not config["node"]:
@@ -232,7 +229,7 @@ class Steem(object):
             :param bool nobroadcast: Do **not** broadcast a transaction!
 
             If no node is provided, it will connect to the node of
-            SteemIT.com. It is **highly** recommended that you pick your own
+            http://piston.rocks. It is **highly** recommended that you pick your own
             node instead. Default settings can be changed with:
 
             .. code-block:: python
@@ -242,9 +239,18 @@ class Steem(object):
             where ``<host>`` starts with ``ws://`` or ``wss://``.
         """
 
-        node = kwargs.pop("node", False)
-        rpcuser = kwargs.pop("rpcuser", "")
-        rpcpassword = kwargs.pop("rpcpassword", "")
+        node = None
+        rpcuser = None
+        rpcpassword = None
+        if len(args):
+            node = args.pop(0)
+        if len(args):
+            rpcuser = args.pop(0)
+        if len(args):
+            rpcpassword = args.pop(0)
+        node = kwargs.pop("node", node)
+        rpcuser = kwargs.pop("rpcuser", rpcuser)
+        rpcpassword = kwargs.pop("rpcpassword", rpcpassword)
 
         if not node:
             if "node" in config:
